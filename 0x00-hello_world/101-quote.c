d709b27ffa1fd3acd208be3fe11819099b834aa0
Kesewaa-omari <Kesewaaomari95@gmail.com>
2023-04-20 22:42:22 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main- A program that prints a line to the standard error.
- * Return: Always 1(yes)
+ * main -A program that prints a line to the standard error
+ *
+ * Return: 1 (yes)
  */
 int main(void)
 {
 	write(2,
-"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	Return(1);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
