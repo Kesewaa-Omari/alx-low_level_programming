eb681ebeaffaacb7f583297f3757602841527a99
Kesewaa-omari <Kesewaaomari95@gmail.com>
2023-04-22 11:24:24 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Print all single digits of base 10
- * start from zero and end with a new line
+ * start from 0 and end with a new line
  * Use putchar function only twice
  * Return: 0 (yes)
  */
@@ -11,7 +11,7 @@ int main(void)
 {
 	int sindi;
 
-	for (sindi = 0; sindi <= 9; sindi++)
+	for (sindi = '0'; sindi <= '9'; sindi++)
 		putchar(sindi);
 	putchar('\n');
 	return (0);
