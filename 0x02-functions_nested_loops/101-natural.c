b2da78c0171e709e9f1dda4a8ee0c2436560e882
Kesewaa-omari <Kesewaaomari95@gmail.com>
2023-04-26 13:39:22 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: Always (yes)
+ */
+
+int main(void)
+
+{
+	int x;
+	int y;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			y <= x;
+		}
+		x++;
+	}
+	printf("%d\n", y);
+	return (0);
+}
