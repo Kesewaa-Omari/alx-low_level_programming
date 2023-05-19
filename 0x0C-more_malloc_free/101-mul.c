b2ee5adc48c377eb5094383ba21bef0335649a3b
Kesewaa-omari <Kesewaaomari95@gmail.com>
2023-05-18 20:40:12 -0700

    Task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @str: input string
+ *
+ * Return: 0 if a non-digit is found else 1
+ */
+
+int is_digit(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: input
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _error - checks errors in  main
+ */
+
+void _error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: always 0 (yes)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, d1, d2, *result, x = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		_error();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		d1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			d2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (d1 * d2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			x = 1;
+		if (x)
+			_putchar(result[i] + '0');
+	}
+	if (!x)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
+}
