9c5db82fa924308d5550adade8261760b53c067b
Maame Yaa Kesewaa Omari <117319465+Kesewaa-Omari@users.noreply.github.com>
2023-05-24 14:20:59 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name.
+ * @name: input name
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+	f(name);
+}
