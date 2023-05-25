aa9baf096e52659c444a8aac8d73090bbc99d17f
Maame Yaa Kesewaa Omari <117319465+Kesewaa-Omari@users.noreply.github.com>
2023-05-25 03:08:04 +0000

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - sum of integers
+ * @a: input first integer
+ * @b: input second integer
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - difference of integers
+ * @a: input first integer
+ * @b: input second integer
+ *
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - product of integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - division of integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division of a by b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+	}
+
+/**
+ * op_mod - remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
